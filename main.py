e3f136c23bcaac07e693ebce18eca15d83f0dcf5
themuku <murad.m@develops.today>
2024-04-28 14:11:37 +0400

    draw hand

--- main.py
+++ main.py
@@ -8,16 +8,40 @@ turtle.tracer(0)
 uzbek = turtle.Turtle()
 
 
-def draw_square(x, y, length):
+def draw_lines(length, rotation):
     uzbek.penup()
     uzbek.home()
-    uzbek.goto((x, y))
-    uzbek.color("red")
+    uzbek.color("black")
+    uzbek.pensize(1)
+    uzbek.right(rotation)
+    uzbek.forward(170)
     uzbek.pendown()
+    uzbek.forward(length)
+    uzbek.penup()
 
-    for storona in range(4):
-        uzbek.forward(length)
-        uzbek.right(90)
+
+
+def draw_watchface():
+    uzbek.penup()
+
+    for i in range(0,360,30):
+        draw_lines(20, i)
+
+    for i in range(0,360,6):
+        draw_lines(10, i)
+
+    uzbek.penup()
+    
+
+def draw_hands(length, rotation):
+    uzbek.penup()
+    uzbek.home()
+    uzbek.pensize(2)
+    uzbek.right(1* rotation + 90 * 3)
+    uzbek.pendown()
+    uzbek.forward(length)
+    uzbek.penup()
+
 
 
 turtle.done()
