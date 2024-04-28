f12ab63a2d57f5e45273559959fb1bf96a981549
themuku <murad.m@develops.today>
2024-04-28 13:39:06 +0400

    draw square fn

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import turtle
 
 
-uzbek = turtle.Turtle()
-
 turtle.setup(500, 500, 0, 0)
 turtle.screensize(480, 480, bg="#ccc")
 turtle.tracer(0)
-turtle.hideturtle()
+
+uzbek = turtle.Turtle()
+
+
+def draw_square(x, y, length):
+    uzbek.penup()
+    uzbek.home()
+    uzbek.goto((x, y))
+    uzbek.color("red")
+    uzbek.pendown()
+
+    for storona in range(4):
+        uzbek.forward(length)
+        uzbek.right(90)
 
 
 turtle.done()
